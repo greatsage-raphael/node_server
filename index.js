298bceca73da4460220e064332640b3a4b78de8b
Badilisha Productions <badilishaproductions@Badilishas-MacBook-Pro.local>
2022-09-29 14:33:32 +0300

    First route

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("We're live and in business")
+var http = require('http');
+var url = require('url');
+
+var store = {};
+
+http.createServer(function (req, res) {
+    if (req.url == '/set') { //check the URL of the current request
+
+    const parsedUrl = url.parse(req.url, true).query;
+
+    for(key in parsedUrl){
+    	store[key] = parsedUrl[key];
+    }
+        
+    // set response header
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(store);
+        
+    }
+}).listen(4000);
